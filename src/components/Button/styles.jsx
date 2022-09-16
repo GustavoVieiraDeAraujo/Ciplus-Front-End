abc9f4895bbf50aa529b9a726aefc8eba2c15617
Gustavo <gustavova2003@hotmail.com>
2022-09-16 10:55:46 -0300

    Criando componente de forms sem CSS e ajustando os componentes de Button e Input

--- src/components/Button/styles.jsx
+++ src/components/Button/styles.jsx
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 
 export const StyleButton = styled.button`
     // atributos que podem ser modificados por prop
-    margin: ${props => props.margin ? props.margin : "0" };
-    width: ${props => props.width ? props.width : 'max-content' };
+    margin: ${props => props.margin ? props.margin : "auto" };
+    width: ${props => props.width ? props.width : '100%' };
     color: ${props => props.color ? props.color : "white" };
     height: ${props => props.height ? props.height : 'auto' };
-    background-color: ${props => props.color ? props.color : 'blue' };
+    background-color: ${props => props.color ? props.color : 'black' };
 
     // atributos padrões
     cursor: pointer;
