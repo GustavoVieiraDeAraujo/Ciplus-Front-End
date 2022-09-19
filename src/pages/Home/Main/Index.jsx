e33b135defa1dc6502dc62e94a6806adb49372ab
marlonalvss <marlon14br@gmail.com>
2022-09-19 15:59:50 -0300

    :goal_net:

--- src/pages/Home/Main/Index.jsx
+++ src/pages/Home/Main/Index.jsx
@@ -11,47 +11,48 @@ function Main() {
 
 
     return (
-    <StyleMain>
-        <div className="grid-item fotoCa">
-            <img className="FotoFundo" src={alice} />
-        </div>
-        <div className="grid-item ExpC">
-            <h1 className="enfatiza">Um novo jeito de experimentar o cinema.</h1>
-            <p className="fSize">Conheça nossos planos e economize na assinatura trimestral ou anual com o pagamento antecipado.</p>
-        </div>
-        <div className="divDaImg1">
-        <img className="imgIngresso" src={oculos}/>
-        </div>
-        <section className="grid-item Cards">
-            <div className="card1 corpoC">PREÇOS1</div>
-            <div className="card2 corpoC">PREÇOS2</div>
-            <div className="card3 corpoC">PREÇOS3</div>
-            <div className="card4 corpoC">PREÇOS4</div>
-        </section>
-        <div className="divDaImg2">
-            <img className="imgPipoca" src={pipoca}/>
-        </div>
-        <div className="grid-item item1">
-            <div className="item1c enfatizaUP"><h2 className="enfatiza">Assista a lançamentos!</h2>
-
-               <p> Seja o primeiro a saber o próximo filme a ser lançado!</p> 
-               </div>
-            <img className="dimensoes" src={wanda} />
-        </div>
-        <div className="grid-item item2">
-            <img className="dimensoes2" src={moana} />
-            <div className="enfatizaUP"><h2 className="enfatiza">Crie perfis para crianças.</h2>
-               <p> Deixe as crianças se aventurarem com seus personagens favoritos em um espaço feito só para elas, sem pagar a mais por isso.</p>
-               </div>
-            </div>
-        <div className="grid-item Fp">
-            <p className="FraEf">Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.</p>
-            <form><input placeholder="teste" className="TesteInp" />
-            <button className="Vl">Vamos lá!</button>
-            </form> 
-        </div>
-    </StyleMain>
-    ) 
+        <StyleMain>
+            <figure className="grid-item fotoCa">
+                <img className="FotoFundo" src={alice} />
+            </figure>
+            <section className="grid-item ExpC">
+                <h1 className="enfatiza">Um novo jeito de experimentar o cinema.</h1>
+                <p className="fSize">Conheça nossos planos e economize na assinatura trimestral ou anual com o pagamento antecipado.</p>
+            </section>
+            <figure className="divDaImg1">
+                <img className="imgIngresso" src={oculos} />
+            </figure>
+            <section className="grid-item Cards">
+                <div className="card1 corpoC">PREÇOS1</div>
+                <div className="card2 corpoC">PREÇOS2</div>
+                <div className="card3 corpoC">PREÇOS3</div>
+                <div className="card4 corpoC">PREÇOS4</div>
+            </section>
+            <figure className="divDaImg2">
+                <img className="imgPipoca" src={pipoca} />
+            </figure>
+            <section className="grid-item item1">
+                <article className="item1c enfatizaUP">
+                    <h2 className="enfatiza">Assista a lançamentos!</h2>
+                    <p> Seja o primeiro a saber o próximo filme a ser lançado!</p>
+                </article>
+                <img className="dimensoes" src={wanda} />
+            </section>
+            <section className="grid-item item2">
+                <img className="dimensoes2" src={moana} />
+                <article className="enfatizaUP">
+                    <h2 className="enfatiza">Crie perfis para crianças.</h2>
+                    <p> Deixe as crianças se aventurarem com seus personagens favoritos em um espaço feito só para elas, sem pagar a mais por isso.</p>
+                </article>
+            </section>
+            <section className="grid-item Fp">
+                <p className="FraEf">Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.</p>
+                <form><input placeholder="teste" className="TesteInp" />
+                    <button className="Vl">Vamos lá!</button>
+                </form>
+            </section>
+        </StyleMain>
+    )
 }
 
 export default Main
