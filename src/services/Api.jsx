0725b4c85f7f88a5a8ef6e79ebb2240c4da847c2
Isabella Oliveira <92725386+isabellaoliv@users.noreply.github.com>
2022-09-24 09:42:18 -0300

    Permitindo configurar a URL da API por variavel de ambiente

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://ciplus-back-end.herokuapp.com/",
+    baseURL: import.meta.env.VITE_API_URL || "https://ciplus-back-end.herokuapp.com/",
 });
 
 export const GetAll = async(entidade) => {
