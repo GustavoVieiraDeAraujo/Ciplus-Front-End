39ac2397cfcc3936b2d43a1108868416def29612
Gustavo <gustavova2003@hotmail.com>
2022-09-30 14:35:58 -0300

    Trocando a URL de fallback do Heroku (fora do ar) por localhost

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "https://ciplus-back-end.herokuapp.com/",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/",
 });
 
 // O token fica em localStorage (lembrar de mim) ou sessionStorage (sessao atual),
